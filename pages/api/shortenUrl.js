3d5ba378b63bc486ab88f3d6fa4ef0b510383e5c
ankush <webmaster.ankush@gmail.com>
2024-08-07 08:16:59 +0530

    Update shortenUrl.js

--- pages/api/shortenUrl.js
+++ pages/api/shortenUrl.js
@@ -1,25 +1,7 @@
-// pages/api/shortenUrl.js
-
-import { BitlyClient } from 'bitly';
-
-const bitly = new BitlyClient('068dfecf9be53747723678426ca6758a0c9df94d', {});
-
 export default async function handler(req, res) {
-    console.log('inside handler');
-    
-    if (req.method === 'POST') {
-        try {
-            const body = JSON.parse(req.body);
-            const { longUrl } = body;
+  console.log('inside handler');
+  const body = JSON.parse(req.body);
+  const { longUrl } = body;
 
-            const response = await bitly.shorten(longUrl);
-            res.status(200).json({ shortUrl: response.link });
-        } catch (error) {
-            console.error('Error shortening URL:', error);
-            res.status(500).json({ error: 'Failed to shorten URL' });
-        }
-    } else {
-        res.setHeader('Allow', ['POST']);
-        res.status(405).end(`Method ${req.method} Not Allowed`);
-    }
+  res.status(200).json({ data: longUrl });
 }
